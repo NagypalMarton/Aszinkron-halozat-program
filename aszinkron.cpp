cc5323e680c13a35ab3a34da867fc9e39a8e7a0f
NagypalMarton <43655988+NagypalMarton@users.noreply.github.com>
2020-02-21 21:52:07 +0100

    20200221
    Első alfa verzió

---
+++ aszinkron.cpp
@@ -0,0 +1,72 @@
+/*lav[sor][oszlop]
+lavdb -> lényeges állapotváltozások darab száma
+negdb -> negáció darabszáma
+*/
+
+#include<iostream>
+#include <math.h>
+using namespace std;
+
+	int main(int argc, char* argv[])
+	{
+		unsigned int lavdb, negdb, oszlop = 4;
+		char igen = 'y', valasz;
+
+		setlocale(LC_ALL, "HUN");
+		cout << "Aszinkron sorrendi hálózat\n" << "(Create by Nagypál Márton [Neptun kód: B3081T])\n\n";
+
+		cout << "\tLesz-e negáció a LÉNYEGES állapotváltozásban? (y/n) \t";
+		cin >> valasz;
+		if (valasz == igen)
+		{
+			do
+			{
+				cout << "\tHány darab lesz benne? (max 3, min 1 lehet)\t\t";
+				cin >> negdb;
+				if (negdb < 1 || negdb>3)
+				{
+					cout << "\n\t\tNem megfelelõ számot írtál be! Írj be újat!\n\n";
+				}
+			} while (negdb<1 || negdb>3);
+			
+			cout << endl;
+			lavdb = pow(2, negdb); //hatványozás
+			int **lav = new int*[lavdb]; //dinamikus  tömb létrehozás SOR LÉTREHOZÁSA
+
+			//lav tömb kiterjesztése több dimenzióssá tétele
+			for (int i = 0; i <= lavdb - 1; i++)
+			{
+				lav[i] = new int[oszlop];//OSZLOPot hozza létre
+			}
+		}
+		else
+		{
+			do
+			{
+				cout << "\tHány LÉNYEGES állapotváltozás lesz? (1 és 4 lehet)\t";
+				cin >> lavdb;
+				if (lavdb<1 || lavdb>4)
+				{
+					cout << "\n\t\tNem megfelelõ számot írtál be! Írj be újat!\n\n";
+				}
+			} while (lavdb < 1 || lavdb>4);
+			
+			cout << endl;
+			int **lav = new int*[lavdb]; //dinamikus  tömb létrehozás SOR LÉTREHOZÁSA
+
+			//lav tömb kiterjesztése több dimenzióssá tétele
+			for (int i = 0; i <= lavdb - 1; i++)
+			{
+				lav[i] = new int[oszlop]; //OSZLOPot hozza létre
+			}
+		}
+
+		for (int i = 0; i < lavdb; i++)
+		{
+			cout << i+1 << "\t";
+			//cout << "\tKérem a(z) " << i++ << " értékét:\t" << endl;
+		}
+		cout << endl;
+	system("pause");
+	return 0;
+}
